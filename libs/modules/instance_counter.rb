f16904d470e5c7eac4112201f682a24534afb676
elbub1979 <ilya.leuh09@gmail.com>
2024-09-20 12:22:03 +0300

    refactoring instance_counter

--- libs/modules/instance_counter.rb
+++ libs/modules/instance_counter.rb
@@ -5,16 +5,16 @@ module InstanceCounter
     base.send :include, InstanceMethods
   end
 
-  @@instances_count
+  @@instances_count = 0
 
   module ClassMethods
-
     def instances
       InstanceCounter.class_variable_get '@@instances_count'
     end
   end
 
   module InstanceMethods
+    private
     def register_instance
       current_count = InstanceCounter.class_variable_get '@@instances_count'
       InstanceCounter.class_variable_set '@@instances_count', current_count + 1
