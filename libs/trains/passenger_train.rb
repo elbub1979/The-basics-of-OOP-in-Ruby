b9b3db8a4cbfd153b48a5f1016436b03bcbf06a2
elbub1979 <ilya.leuh09@gmail.com>
2024-10-10 22:04:46 +0300

    manual editing according to style guide in passenger train class

--- libs/trains/passenger_train.rb
+++ libs/trains/passenger_train.rb
@@ -4,8 +4,6 @@ class PassengerTrain < Train
   def add_wagon(wagon)
     raise 'Wrong wagon: not pass' unless wagon.instance_of?(PassengerWagon)
 
-    # return 'Wrong wagon: not pass' unless wagon.instance_of?(PassengerWagon)
-
     super
   end
 end
