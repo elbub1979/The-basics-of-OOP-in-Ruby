53234131aa6d3aa7b05d2693ec9c83271a4f30b7
elbub1979 <ilya.leuh09@gmail.com>
2024-10-10 22:05:10 +0300

    manual editing according to style guide in cargo train class

--- libs/trains/cargo_train.rb
+++ libs/trains/cargo_train.rb
@@ -4,8 +4,6 @@ class CargoTrain < Train
   def add_wagon(wagon)
     raise 'Wrong wagon: not cargo' unless wagon.instance_of?(CargoWagon)
 
-    # return 'Wrong wagon: not cargo' unless wagon.instance_of?(CargoWagon)
-
     super
   end
 end
